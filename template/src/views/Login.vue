ef1fb391fb32d7081a67b75373e44f8de5038198
LuoHuacheng <154224099@qq.com>
2018-05-15 16:00:48 +0800

    页面文件夹

---
+++ template/src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login-form">
+    <Logo></Logo>
+    <input type="text" v-model="username" />
+    <input type="text" v-model="password" />
+    <input type="text" v-model="verifyCode" />
+    <button type="button" @click="handleLogin">登录</button>
+  </div>
+</template>
+
+<script>
+import Logo from '../components/logo';
+import { login } from '../api/fetchData';
+
+export default {
+  name: 'Login',
+  components: { Logo },
+  data() {
+    return {
+      username: '',
+      password: '',
+      verifyCode: '',
+    };
+  },
+  methods: {
+    handleLogin() {
+      const { username, password, verifyCode } = this;
+      login({ username, password, verifyCode });
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
